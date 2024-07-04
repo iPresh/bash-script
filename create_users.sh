bd25355fe36848d15f2db0592d878ed33e45a35c
iPresh <amuhprecious@gmail.com>
2024-07-04 17:23:04 +0100

    modified script

--- create_users.sh
+++ create_users.sh
@@ -4,7 +4,7 @@
 LOG_FILE="/var/log/user_management.log"
 PASSWORD_FILE="/var/secure/user_passwords.csv"
 
-# create directories and files, then assign appropriate permissions
+# Create directories and files, then assign appropriate permissions
 sudo mkdir -p /var/log
 sudo mkdir -p /var/secure
 sudo touch "$LOG_FILE"
@@ -18,11 +18,11 @@ generate_password() {
 
 # Read the input file
 while IFS=";" read -r username groups; do
-  # eliminate whitespace
+  # Eliminate whitespace
   username=$(echo "$username" | xargs)
   groups=$(echo "$groups" | xargs)
 
-  # Create a user group, then a primary group using username
+  # Create a user-specific primary group
   if ! getent group "$username" > /dev/null; then
     sudo groupadd "$username"
     echo "$(date): Group $username created" | sudo tee -a "$LOG_FILE"
@@ -30,19 +30,19 @@ while IFS=";" read -r username groups; do
     echo "$(date): Group $username already exists" | sudo tee -a "$LOG_FILE"
   fi
 
-  # Create user if it doesn't exist
+  # Create the user if it doesn't exist
   if ! id -u "$username" > /dev/null 2>&1; then
-    sudo useradd -m -g "$username" -G "$groups" "$username"
+    sudo useradd -m -g "$username" "$username"
     password=$(generate_password)
     echo "$username:$password" | sudo chpasswd
-    echo "$(date): User $username created with groups $groups" | sudo tee -a "$LOG_FILE"
+    echo "$(date): User $username created" | sudo tee -a "$LOG_FILE"
     echo "$username,$password" | sudo tee -a "$PASSWORD_FILE"
     sudo chown root:root "$PASSWORD_FILE"
   else
     echo "$(date): User $username already exists" | sudo tee -a "$LOG_FILE"
   fi
 
-  # Create additional groups
+  # Create and assign additional groups
   IFS=',' read -ra ADDR <<< "$groups"
   for group in "${ADDR[@]}"; do
     if ! getent group "$group" > /dev/null; then
